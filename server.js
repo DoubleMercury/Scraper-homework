e09a256dd5dbe9c33494652f83116a314d2dc75b
Double Mercury <maafakeout@yahoo.com>
2019-06-13 10:20:21 -0400

    changed port settings to try and get heroku to work

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gameinformer";
 mongoose.connect(MONGODB_URI);
 
 
-var PORT = 3000;
+var PORT = process.env.PORT ||3000;
 
 // Initialize Express
 var app = express();
